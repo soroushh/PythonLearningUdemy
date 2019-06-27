633901820d68ff55f3227fa334916357ad01a746
student <sorush.kh68@gmail.com>
2019-06-27 10:25:02 +0100

    We can do list and tuple operations.

--- lists_tuples_sets.py
+++ lists_tuples_sets.py
@@ -4,10 +4,18 @@ tuple_list = (1,2,3)
 
 set_list = { 1,2,3 }
 
+my_list.append("hello")
 
+print(my_list)
 
-print(sum(my_list))
+tuple_list = tuple_list + (200,)
 
-print(len(my_list))
+print(tuple_list)
 
-print(sum(my_list)/len(my_list))
+my_list[0] = 60
+
+print(my_list)
+
+tuple_list[0] = 13
+
+print(tuple_list)
