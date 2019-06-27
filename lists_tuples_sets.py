98e52013e3016ad33a23750f2f705bdd0a538d3a
student <sorush.kh68@gmail.com>
2019-06-27 10:32:12 +0100

    We can do set operations.

--- lists_tuples_sets.py
+++ lists_tuples_sets.py
@@ -4,18 +4,33 @@ tuple_list = (1,2,3)
 
 set_list = { 1,2,3 }
 
-my_list.append("hello")
+set_list.add(12)
 
-print(my_list)
+print(set_list)
 
-tuple_list = tuple_list + (200,)
 
-print(tuple_list)
+set_list.add(2)
 
-my_list[0] = 60
+print(set_list)
 
-print(my_list)
+set_list[0] = 10
 
-tuple_list[0] = 13
+print(set_list)
 
-print(tuple_list)
+
+
+# my_list.append("hello")
+#
+# print(my_list)
+#
+# tuple_list = tuple_list + (200,)
+#
+# print(tuple_list)
+#
+# my_list[0] = 60
+#
+# print(my_list)
+#
+# tuple_list[0] = 13
+#
+# print(tuple_list)
