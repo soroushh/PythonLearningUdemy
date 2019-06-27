f59ba2b65a4db3c505371880d96ba884dbae6610
student <sorush.kh68@gmail.com>
2019-06-27 16:54:23 +0100

    We can iterate over dictionaries.

--- dics.py
+++ dics.py
@@ -3,4 +3,14 @@ my_dict = {"soroush":"khosravi", "farnaz":"khosravi"}
 for name,family in my_dict.items() :
     print(name +" "+ family)
 
-     
+universities = [
+{"name":"MIT",
+"location": "USA"},
+{
+"name":"Sharif",
+"location": "Iran"
+}
+]
+
+for uni in universities:
+    print("The" + uni["name"] + "is located in " + uni["location"])
