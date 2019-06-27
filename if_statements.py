e155b2b41781b7865c28967e5f2154053e30e6b1
student <sorush.kh68@gmail.com>
2019-06-27 11:49:09 +0100

    We can use if statements in python. in addition, we can use string interpolation in python.

---
+++ if_statements.py
@@ -0,0 +1,13 @@
+this_true = True
+
+if this_true :
+    print("This-true is true.")
+
+names =["soroush", "kati", "rostam"]
+
+person = input("Person you know?")
+
+if person in names :
+    print("You know {}." .format(person))
+else:
+    print("You don't know {}." .format(person))
