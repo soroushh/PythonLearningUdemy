b4d44d3c6424753113142b05bd0a211422b27e67
student <sorush.kh68@gmail.com>
2019-06-27 11:40:07 +0100

    We can use while and for loops.

--- lists_tuples_sets.py
+++ lists_tuples_sets.py
@@ -1,47 +1,47 @@
-# my_list = [1,2,3]
-#
-# tuple_list = (1,2,3)
-#
-# set_list = { 1,2,3 }
-#
-# set_list.add(12)
-#
-# print(set_list)
-#
-#
-# set_list.add(2)
-#
-# print(set_list)
-#
-# set_list[0] = 10
-#
-# print(set_list)
-#
-# my_list.append("hello")
-#
-# print(my_list)
-#
-# tuple_list = tuple_list + (200,)
-#
-# print(tuple_list)
-#
-# my_list[0] = 60
-#
-# print(my_list)
-#
-# tuple_list[0] = 13
-#
-# print(tuple_list)
+my_list = [1,2,3]
 
-# your_lottory_numbers = {1,2,3,4,5}
-#
-# winning_numbers = {1,3,5,7,9,11}
-#
-# print(your_lottory_numbers.intersection(winning_numbers))
-#
-# print(your_lottory_numbers.union(winning_numbers))
-#
-# print(your_lottory_numbers.difference(winning_numbers))
+tuple_list = (1,2,3)
+
+set_list = { 1,2,3 }
+
+set_list.add(12)
+
+print(set_list)
+
+
+set_list.add(2)
+
+print(set_list)
+
+set_list[0] = 10
+
+print(set_list)
+
+my_list.append("hello")
+
+print(my_list)
+
+tuple_list = tuple_list + (200,)
+
+print(tuple_list)
+
+my_list[0] = 60
+
+print(my_list)
+
+tuple_list[0] = 13
+
+print(tuple_list)
+
+your_lottory_numbers = {1,2,3,4,5}
+
+winning_numbers = {1,3,5,7,9,11}
+
+print(your_lottory_numbers.intersection(winning_numbers))
+
+print(your_lottory_numbers.union(winning_numbers))
+
+print(your_lottory_numbers.difference(winning_numbers))
 
 my_set = (100)
 
---
+++ loop.py
@@ -0,0 +1,15 @@
+my_variable = "hello"
+
+
+for letter in my_variable:
+    print(letter)
+
+user_wants_true = True
+
+counter = 0
+
+while counter <= 5 :
+    if user_wants_true == True:
+        print("still true")
+        counter += 1
+print("it is over")
