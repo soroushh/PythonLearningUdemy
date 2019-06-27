33261197f2287f62a64a5ea3f8efa59d0b9e5250
student <sorush.kh68@gmail.com>
2019-06-27 16:49:21 +0100

    We can use dics in python now.

---
+++ dics.py
@@ -0,0 +1,6 @@
+my_dict = {"soroush":"khosravi", "farnaz":"khosravi"}
+
+for name,family in my_dict.items() :
+    print(name +" "+ family)
+
+     
---
+++ list_comprehension.py
@@ -0,0 +1,7 @@
+numbers_list = [number for number in range(5)]
+
+print(numbers_list)
+
+names_you_know = [name.strip() for name in input("Enter names").split(",")]
+
+print(names_you_know)
