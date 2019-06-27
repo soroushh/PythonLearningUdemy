993f05116c1daab2f345728c821b6cfed82ce75d
student <sorush.kh68@gmail.com>
2019-06-27 11:18:09 +0100

    We can do set operations.

--- lists_tuples_sets.py
+++ lists_tuples_sets.py
@@ -1,24 +1,22 @@
-my_list = [1,2,3]
-
-tuple_list = (1,2,3)
-
-set_list = { 1,2,3 }
-
-set_list.add(12)
-
-print(set_list)
-
-
-set_list.add(2)
-
-print(set_list)
-
-set_list[0] = 10
-
-print(set_list)
-
-
-
+# my_list = [1,2,3]
+#
+# tuple_list = (1,2,3)
+#
+# set_list = { 1,2,3 }
+#
+# set_list.add(12)
+#
+# print(set_list)
+#
+#
+# set_list.add(2)
+#
+# print(set_list)
+#
+# set_list[0] = 10
+#
+# print(set_list)
+#
 # my_list.append("hello")
 #
 # print(my_list)
@@ -34,3 +32,13 @@ print(set_list)
 # tuple_list[0] = 13
 #
 # print(tuple_list)
+
+your_lottory_numbers = {1,2,3,4,5}
+
+winning_numbers = {1,3,5,7,9,11}
+
+print(your_lottory_numbers.intersection(winning_numbers))
+
+print(your_lottory_numbers.union(winning_numbers))
+
+print(your_lottory_numbers.difference(winning_numbers))
