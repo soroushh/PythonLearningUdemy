42bf7da9839f2ccb54c67b31df98a22e15febec1
student <sorush.kh68@gmail.com>
2019-06-27 18:41:37 +0100

    We can define class methods too.

--- classes_objects.py
+++ classes_objects.py
@@ -1,10 +1,16 @@
 class Person:
+    all_objects = []
     def __init__(self, name, family):
         self.name = name
         self.family = family
+        Person.all_objects.append(self)
 
     def full_name(self):
         return(self.name + " "+ self.family)
+
+    @classmethod
+    def all(cls):
+        return(cls.all_objects)
 
 person = Person("soroush","khosravi")
 
@@ -13,3 +19,9 @@ print(person.full_name())
 print(person.name)
 
 print(person.family)
+
+print(Person.all())
+
+person_2 = Person("farnaz","ostovari")
+
+print(Person.all())
