b599db1ec896266bd7e0ddccbf628bc5e3b4e083
student <sorush.kh68@gmail.com>
2019-06-28 22:00:54 +0100

    We did something.

--- classes_objects.py
+++ classes_objects.py
@@ -12,30 +12,12 @@ class Person:
     def all(cls):
         return(cls.all_objects)
 
-    @classmethod
-    def find_by_name(cls,name):
-        return([person for person in cls.all() if person.name == name])
-
-
 person = Person("soroush","khosravi")
 
 print(person.full_name())
 
+person.name = "farnaz"
+
+print(person.full_name())
+
 print(person.name)
-
-print(person.family)
-
-print(Person.all())
-
-person_2 = Person("farnaz","ostovari")
-
-print(Person.all())
-
-print(Person.find_by_name("farnaz"))
-
-class A(Person):
-    pass
-
-
-
-print(A.all())
